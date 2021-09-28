12c4dc5f1bd6c716291b6354e7958723b25320c7
mahdeesharef <71808546+mahdeesharef@users.noreply.github.com>
2021-09-29 00:32:45 +0300

    add cheng

--- task.js
+++ task.js
@@ -10,12 +10,11 @@ let numbers = [
 ];
 
 console.log("Original numbers list: ", numbers)
-
-for(let i = 0 ; i < numbers.length ; i++)
+for(let i =1 ; i < numbers.length ; i++) // i=1 instead of ‏    0
 {
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
+    for(let j =0 ; j < numbers.length-1; j++)
     {
-        if(numbers[i] > numbers[j + 1])
+        if(numbers[j] < numbers[j +1]) // numbers[i] to numbers[j]  compar comper btwen j and j+1 
         {
             let temp = numbers[j];
             numbers[j] = numbers[j + 1]
@@ -24,13 +23,15 @@ for(let i = 0 ; i < numbers.length ; i++)
     }
 }
 
+
+
 console.log("Numbers list After sorting: ", numbers)
 
-for(let i = 0 ; i < numbers.length ; i++)
+for(let i = 1 ; i < numbers.length ; i++)  // i=1 instead of ‏  0
 {
     for(let j = 0 ; j < numbers.length - 1 ; j++)
     {
-        if(numbers[i] < numbers[j + 1])
+        if(numbers[j] > numbers[j + 1]) // numbers[i] to numbers[j]  compar comper btwen j and j+1 
         {
             let temp = numbers[j];
             numbers[j] = numbers[j + 1]
